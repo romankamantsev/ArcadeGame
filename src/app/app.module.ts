728c2b6b510c08c7b5c04c33449cfd0052f1a3b8
romankamantsev <roman.kamantsev@gmail.com>
2021-01-14 11:32:51 +0200

    moved header component to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header.component";
-import { HistoryComponent } from './history/history.component';
-import { BalanceComponent } from './balance/balance.component';
-import { RefillComponent } from './refill/refill.component';
+import { HeaderComponent } from "./component/header.component";
+import { HistoryComponent } from "./history/history.component";
+import { BalanceComponent } from "./balance/balance.component";
+import { RefillComponent } from "./refill/refill.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent, HistoryComponent, BalanceComponent, RefillComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HistoryComponent,
+    BalanceComponent,
+    RefillComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
